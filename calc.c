f307e84aa5cdea1d0598509e5c62c80942d2b31a
jordanklahr <jordanklahr@gmail.com>
2017-09-13 20:50:16 -0400

    Final

--- calc.c
+++ calc.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
 	//printf("%s %s\n",argv[2],argv[3]);
 	float numb1 = atof(argv[2]);
 	float numb2 = atof(argv[3]);
-	printf("%f %f\n",numb1,numb2);
+	//printf("%f %f\n",numb1,numb2);
 	float ans;
-	if(argv[1] == "+")
+	if(strncmp(argv[1], "+",1) == 0)
 	{
 		ans = numb1 + numb2;
 	}
-        if(argv[1] == "-")
+        if(strncmp(argv[1], "-",1) == 0)
         {
                 ans = numb1 - numb2;
         }
-        if(argv[1] == "x")
+        if(strncmp(argv[1], "x",1) == 0)
         {
                 ans = numb1 * numb2;
         }
-        if(argv[1] == "/")
+        if(strncmp(argv[1], "/",1) == 0)
         {
                 ans = numb1 / numb2;
         }
-	printf("%f\n",ans);
+	printf("%g\n",ans);
 }
